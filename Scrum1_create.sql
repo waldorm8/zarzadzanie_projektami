76eb1f2af6e7e2613919fba4403968ec709e3f74
Kowal93 <tomasz.kowalski182@gmail.com>
2017-03-12 23:11:47 +0100

    Add files via upload

--- Scrum1_create.sql
+++ Scrum1_create.sql
@@ -1,10 +1,10 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-03-12 22:03:04.951
+-- Last modification date: 2017-03-12 22:11:09.776
 
 -- tables
 -- Table: Uzytkownicy
 CREATE TABLE Uzytkownicy (
-    id_uzytkownika int NOT NULL,
+    id_uzytkownika int NOT NULL AUTO_INCREMENT,
     login varchar(50) NOT NULL,
     haslo varchar(50) NOT NULL,
     imie varchar(50) NOT NULL,

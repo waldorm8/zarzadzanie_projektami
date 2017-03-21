2138c87d950b0c5bd1a320e70240561050fe2ef2
piotrek31493 <piotrek31493@wp.pl>
2017-03-21 23:10:18 +0100

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -9,13 +9,16 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
+    <title>Zarządzanie projektem informatycznym</title>
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../dist/css/style.css" rel="stylesheet">
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 
 <body>
+
 <?php 
     require('classes/user.php');
     $logowanie = new User(); //obsluga logowania;
@@ -26,32 +29,64 @@
         $logowanie -> wyloguj();      
     }
 ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
+
+    <div id="wrapper">
+
+        <nav  id="main-navbar" class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">ADMIN</a>
+            </div>
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse" id="menu_b">
+                    <ul class="nav" id="side-menu">
+                        <li class="side_m">
+                            <a href="#" class="side_h"><i class="fa fa-calendar"></i> Kalendarz</a>
+                        </li>
+                        <li class="side_m">
+                            <a href="#" class="side_h"><i class="fa fa-cog"></i> Ustawienia aplikacji</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default" id="login_panel">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
+                        <h3 class="panel-title">Logowanie</h3>
                     </div>
                     <div class="panel-body">
                         <form role="form" method="post" action="login.php">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Login" name="login" type="text" autofocus>
+                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
                                 </div>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                    <input class="form-control" placeholder="Hasło" name="password" type="password" value="">
                                 </div>
                                 <div class="checkbox">
                                     <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                        <input name="remember" type="checkbox" value="Remember Me">Zapamiętaj mnie
                                     </label>
                                 </div>
-                                <input class="btn btn-lg btn-success btn-block" type="submit" value="Zaloguj" />
+                                <a href="#" class="btn btn-lg btn-success btn-block">Zaloguj</a>
+                                <a href="#" class="btn btn-lg btn-success btn-block" style="margin-top:50px;">Nie pamiętasz hasła?</a>
+                                <a href="#" class="btn btn-lg btn-success btn-block">Nie masz konta?</a>
                             </fieldset>
                         </form>
                     </div>
                 </div>
+            </div>
+
             </div>
         </div>
     </div>

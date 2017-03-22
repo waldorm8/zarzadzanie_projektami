3b58abb7253b4ce4be5cdd1b20f214c1937f434d
Arkadiusz Wiśniewski <waldorm8@gmail.com>
2017-03-22 23:55:45 +0100

    obsluga emaila przy logowaniu

--- pages/classes/user.php
+++ pages/classes/user.php
@@ -6,16 +6,18 @@ class User extends Baza {
     function __construct(){
         session_start();
     }
-    function zaloguj_sie($login, $password){
+    function zaloguj_sie($login, $password, $email){
         $baza = new Baza();
         $hashed_password = sha1($password);
         
-        $zapytanie = "SELECT login, haslo FROM uzytkownicy WHERE login = '".$login."' AND haslo = '".$hashed_password."';";
+        $zapytanie = "SELECT login, haslo, email FROM uzytkownicy WHERE login = '".$login."' OR email = '".$email."' AND haslo = '".$hashed_password."';";
         
         if($result = $baza -> link -> query($zapytanie)){
             if($result -> num_rows == 1){
                 $_SESSION['zalogowany'] = 1;
-                $_SESSION['login'] = $login;
+                while($user = $result -> fetch_assoc()){
+                 $_SESSION['login'] = $user['login']; // z zapytania wyciagnac login;   
+                }
                 header('Location: zalogowany/index.php');
             }
             else{
@@ -33,5 +35,4 @@ class User extends Baza {
         //$_SESSION['zalogowany'] = 0;
         session_unset();
     }
-
 }

5450e07d6565aeddc1c37fba15b1088329e10a5d
Arkadiusz Wiśniewski <waldorm8@gmail.com>
2017-04-11 00:10:11 +0200

    forma dodawania projektu
    prototyp

---
+++ pages/zalogowany/dodaj_projekt.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<style>
+		form{
+			margin:0px auto;
+			text-align:left;
+			width:200px;
+			padding:10px;
+		}
+		input, textarea{
+			margin-top:10px;
+		}
+	</style>
+</head>
+<?php 
+require('../classes/user.php');
+$user = new User();
+
+if($user -> sprawdz_log() == True){
+	if(isset($_POST['zapisz'])){
+		$user -> dodaj_projekt($_POST['nazwa_projektu'], $_POST['opis_projektu']);
+	}
+?>
+<body>
+	<form method="post" action="dodaj_projekt.php">
+		<input type="text" name="nazwa_projektu" placeholder="Nazwa projektu" />
+		<textarea name="opis_projektu" placeholder="Krótki opis"></textarea>
+		<input type="submit" value="Zapisz" name="zapisz" />
+	</form>
+<?php 
+}
+else{
+	echo '<meta http-equiv = "refresh" content="1; URL=../login.php">';
+    echo '<p>TAK NIE WOLNO! NIE ZALOGOWALES SIE NIE MASZ DOSTEPU!</p>';
+}
+?>
+</body>
+</html>

c877ca6a681200d06f1a1a753cc98ac259f22a25
Arkadiusz Wiśniewski <waldorm8@gmail.com>
2017-04-10 17:47:23 +0200

    +autentykacja +tabelka projektow

--- pages/zalogowany/index.php
+++ pages/zalogowany/index.php
@@ -1,16 +1,54 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
 <?php 
-/*require('../classes/user.php');
-$zalogowany_user = new User();*/
-session_start();
-if(isset($_SESSION['zalogowany'])){
-    
-    if($_SESSION['zalogowany'] == 1){
-        echo 'jestes zalogowany jako ' . $_SESSION['login'];
-    
-        echo '<br /><a href="../login.php?wylogowany">wyloguj</a>';   
-    }
+require('../classes/user.php');
+$user = new User();
+
+if($user -> sprawdz_log() == True){
+?>
+<p>Witaj <?php $_SESSION['login'] ?></p>
+<p><a href="../login.php?wylogowany">Wyloguj</a>
+<table border="dotted" width="300px;" align="center">
+	<tr>
+		<th>
+			L.p
+		</th>
+		<th>
+			Nazwa projektu
+		</th>
+	</tr>
+	<tr>
+		<td>
+			1
+		</td>
+		<td>
+			Projekt 1
+		</td>
+	</tr>
+	<tr>
+		<td>
+			2
+		</td>
+		<td>
+			Projekt 2
+		</td>
+	</tr>
+</table>
+<?php 
 }
 else{
-    echo '<meta http-equiv = "refresh" content="1; URL=../login.php">';
+	echo '<meta http-equiv = "refresh" content="1; URL=../login.php">';
     echo '<p>TAK NIE WOLNO! NIE ZALOGOWALES SIE NIE MASZ DOSTEPU!</p>';
 }
+?>
+</body>
+</html>

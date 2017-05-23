7d51cd36dd5cd49ea3eb4c98f91d9524647612ce
Arkadiusz Wiśniewski <waldorm8@gmail.com>
2017-05-23 22:19:35 +0200

    Add files via upload
    poprawienie kilku typow

--- baza v0.1.sql
+++ baza v0.1.sql
@@ -1,35 +1,125 @@
-CREATE TABLE `users` (
-	`user_id` int NOT NULL,
-	`login` varchar(15) NOT NULL,
-	`password` varchar(8) NOT NULL,
-	`email` varchar(128),
-	PRIMARY KEY (`user_id`)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 23 Maj 2017, 22:18
+-- Wersja serwera: 10.1.21-MariaDB
+-- Wersja PHP: 7.0.15
 
-CREATE TABLE `projects` (
-	`project_id` int NOT NULL,
-	`title` varchar(255),
-	`status` char,
-	`user_id` int,
-	`description` mediumtext,
-	`date_reg` DATETIME,
-	`date_end` DATETIME,
-	`raiting` DECIMAL,
-	PRIMARY KEY (`project_id`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `ipz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `comments`
+--
 
 CREATE TABLE `comments` (
-	`comment_id` int NOT NULL,
-	`project_id` int,
-	`user_id` int,
-	`date` DATETIME,
-	`comment` TEXT,
-	PRIMARY KEY (`comment_id`)
-);
+  `comment_id` int(11) NOT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `comment` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `projects` ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `comments` ADD CONSTRAINT `comments_fk0` FOREIGN KEY (`project_id`) REFERENCES `projects`(`project_id`);
+--
+-- Struktura tabeli dla tabeli `projects`
+--
 
-ALTER TABLE `comments` ADD CONSTRAINT `comments_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
+CREATE TABLE `projects` (
+  `project_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `status` char(1) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `description` text,
+  `date_reg` date DEFAULT NULL,
+  `date_end` date DEFAULT NULL,
+  `raiting` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Zrzut danych tabeli `projects`
+--
+
+INSERT INTO `projects` (`project_id`, `title`, `status`, `user_id`, `description`, `date_reg`, `date_end`, `raiting`) VALUES
+(0, 'testestesteste', NULL, 0, 'teasdasdsdgsadgsadfsawdfsadfsadf', '2017-05-23', '2017-08-17', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `login` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`user_id`, `login`, `password`, `email`) VALUES
+(0, 'waldorm', 'c30db04d6e2bd577df2ca59eefdf8134f906f9d7', 'waldorm8@gmail.com');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `comments_fk0` (`project_id`),
+  ADD KEY `comments_fk1` (`user_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`project_id`),
+  ADD KEY `projects_fk0` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_fk0` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
+  ADD CONSTRAINT `comments_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Ograniczenia dla tabeli `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

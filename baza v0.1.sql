83d64df801280628b6b52d18c16bc711fad1999e
Kowal93 <tomasz.kowalski182@gmail.com>
2017-06-05 19:21:44 +0200

    Add files via upload

--- baza v0.1.sql
+++ baza v0.1.sql
@@ -1,125 +1,144 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Czas generowania: 23 Maj 2017, 22:18
--- Wersja serwera: 10.1.21-MariaDB
--- Wersja PHP: 7.0.15
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2017-06-05 17:16:43.281
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- tables
+-- Table: allocation
+CREATE TABLE allocation (
+    allocation_id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    project_id int NOT NULL,
+    project_role_id int NOT NULL,
+    CONSTRAINT allocation_pk PRIMARY KEY (allocation_id)
+);
 
+-- Table: project
+CREATE TABLE project (
+    project_id int NOT NULL AUTO_INCREMENT,
+    project_title varchar(255) NOT NULL DEFAULT NULL,
+    project_status char NOT NULL DEFAULT NULL,
+    project_description text NOT NULL,
+    project_start_date date NOT NULL DEFAULT NULL,
+    project_end_date date NOT NULL DEFAULT NULL,
+    project_priority int NOT NULL DEFAULT NULL,
+    CONSTRAINT projects_pk PRIMARY KEY (project_id)
+) ENGINE InnoDB CHARACTER SET utf8;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Table: project_comment
+CREATE TABLE project_comment (
+    project_comment_id int NOT NULL AUTO_INCREMENT,
+    project_id int NOT NULL DEFAULT NULL,
+    user_id int NOT NULL DEFAULT NULL,
+    project_comment_date date NOT NULL DEFAULT NULL,
+    project_comment text NOT NULL,
+    CONSTRAINT comments_pk PRIMARY KEY (project_comment_id)
+) ENGINE InnoDB CHARACTER SET utf8;
 
---
--- Baza danych: `ipz`
---
+-- Table: project_role
+CREATE TABLE project_role (
+    project_role_id int NOT NULL AUTO_INCREMENT,
+    project_role_name varchar(255) NOT NULL,
+    CONSTRAINT project_role_pk PRIMARY KEY (project_role_id)
+);
 
--- --------------------------------------------------------
+-- Table: sprint
+CREATE TABLE sprint (
+    sprint_id int NOT NULL AUTO_INCREMENT,
+    project_id int NOT NULL,
+    sprint_title varchar(255) NOT NULL,
+    sprint_status char NOT NULL,
+    sprint_description text NOT NULL,
+    sprint_start_date date NOT NULL,
+    sprint_end_date date NOT NULL,
+    sprint_priority int NOT NULL,
+    CONSTRAINT sprint_pk PRIMARY KEY (sprint_id)
+);
 
---
--- Struktura tabeli dla tabeli `comments`
---
+-- Table: sprint_comment
+CREATE TABLE sprint_comment (
+    sprint_comment_id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    sprint_id int NOT NULL,
+    sprint_comment_date date NOT NULL,
+    sprint_comment text NOT NULL,
+    CONSTRAINT sprint_comment_pk PRIMARY KEY (sprint_comment_id)
+);
 
-CREATE TABLE `comments` (
-  `comment_id` int(11) NOT NULL,
-  `project_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `comment` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Table: task
+CREATE TABLE task (
+    task_id int NOT NULL AUTO_INCREMENT,
+    sprint_id int NOT NULL,
+    task_title varchar(255) NOT NULL,
+    task_status char NOT NULL,
+    task_description text NOT NULL,
+    task_start_date date NOT NULL,
+    task_end_date date NOT NULL,
+    task_priority int NOT NULL,
+    CONSTRAINT task_pk PRIMARY KEY (task_id)
+);
 
--- --------------------------------------------------------
+-- Table: task_comment
+CREATE TABLE task_comment (
+    task_comment_id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    task_id int NOT NULL,
+    task_comment_date date NOT NULL,
+    task_comment text NOT NULL,
+    CONSTRAINT task_comment_pk PRIMARY KEY (task_comment_id)
+);
 
---
--- Struktura tabeli dla tabeli `projects`
---
+-- Table: users
+CREATE TABLE users (
+    user_id int NOT NULL AUTO_INCREMENT,
+    login varchar(15) NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(255) NOT NULL DEFAULT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (user_id)
+) ENGINE InnoDB CHARACTER SET utf8;
 
-CREATE TABLE `projects` (
-  `project_id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `status` char(1) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `description` text,
-  `date_reg` date DEFAULT NULL,
-  `date_end` date DEFAULT NULL,
-  `raiting` decimal(10,0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- foreign keys
+-- Reference: allocation_project_role (table: allocation)
+ALTER TABLE allocation ADD CONSTRAINT allocation_project_role FOREIGN KEY allocation_project_role (project_role_id)
+    REFERENCES project_role (project_role_id);
 
---
--- Zrzut danych tabeli `projects`
---
+-- Reference: allocation_users (table: allocation)
+ALTER TABLE allocation ADD CONSTRAINT allocation_users FOREIGN KEY allocation_users (user_id)
+    REFERENCES users (user_id);
 
-INSERT INTO `projects` (`project_id`, `title`, `status`, `user_id`, `description`, `date_reg`, `date_end`, `raiting`) VALUES
-(0, 'testestesteste', NULL, 0, 'teasdasdsdgsadgsadfsawdfsadfsadf', '2017-05-23', '2017-08-17', NULL);
+-- Reference: comments_fk0 (table: project_comment)
+ALTER TABLE project_comment ADD CONSTRAINT comments_fk0 FOREIGN KEY comments_fk0 (project_id)
+    REFERENCES project (project_id);
 
--- --------------------------------------------------------
+-- Reference: comments_fk1 (table: project_comment)
+ALTER TABLE project_comment ADD CONSTRAINT comments_fk1 FOREIGN KEY comments_fk1 (user_id)
+    REFERENCES users (user_id);
 
---
--- Struktura tabeli dla tabeli `users`
---
+-- Reference: projects_allocation (table: allocation)
+ALTER TABLE allocation ADD CONSTRAINT projects_allocation FOREIGN KEY projects_allocation (project_id)
+    REFERENCES project (project_id);
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `login` varchar(15) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(128) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Reference: sprint_comments_users (table: sprint_comment)
+ALTER TABLE sprint_comment ADD CONSTRAINT sprint_comments_users FOREIGN KEY sprint_comments_users (user_id)
+    REFERENCES users (user_id);
 
---
--- Zrzut danych tabeli `users`
---
+-- Reference: sprint_project (table: sprint)
+ALTER TABLE sprint ADD CONSTRAINT sprint_project FOREIGN KEY sprint_project (project_id)
+    REFERENCES project (project_id);
 
-INSERT INTO `users` (`user_id`, `login`, `password`, `email`) VALUES
-(0, 'waldorm', 'c30db04d6e2bd577df2ca59eefdf8134f906f9d7', 'waldorm8@gmail.com');
+-- Reference: sprint_sprint_comment (table: sprint_comment)
+ALTER TABLE sprint_comment ADD CONSTRAINT sprint_sprint_comment FOREIGN KEY sprint_sprint_comment (sprint_id)
+    REFERENCES sprint (sprint_id);
 
---
--- Indeksy dla zrzutów tabel
---
+-- Reference: task_comment_task (table: task_comment)
+ALTER TABLE task_comment ADD CONSTRAINT task_comment_task FOREIGN KEY task_comment_task (task_id)
+    REFERENCES task (task_id);
 
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`comment_id`),
-  ADD KEY `comments_fk0` (`project_id`),
-  ADD KEY `comments_fk1` (`user_id`);
+-- Reference: task_comment_users (table: task_comment)
+ALTER TABLE task_comment ADD CONSTRAINT task_comment_users FOREIGN KEY task_comment_users (user_id)
+    REFERENCES users (user_id);
 
---
--- Indexes for table `projects`
---
-ALTER TABLE `projects`
-  ADD PRIMARY KEY (`project_id`),
-  ADD KEY `projects_fk0` (`user_id`);
+-- Reference: task_sprint (table: task)
+ALTER TABLE task ADD CONSTRAINT task_sprint FOREIGN KEY task_sprint (sprint_id)
+    REFERENCES sprint (sprint_id);
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+-- End of file.
 
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_fk0` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
-  ADD CONSTRAINT `comments_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Ograniczenia dla tabeli `projects`
---
-ALTER TABLE `projects`
-  ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

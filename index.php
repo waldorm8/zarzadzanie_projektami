fc04824a92258c712dc17335ed90f6b3f06de5f5
Arkadiusz Wiśniewski <waldorm8@gmail.com>
2017-06-05 19:26:34 +0200

    usuwanie projektow
    + usuwanie projektu

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 
 require('pages/classes/page.php');
 
-if(isset($_GET['page'])){
+if(isset($_GET['usunieto'])){
+
+	require('pages/classes/user.php');
+	$user = new User();
+	if($user -> sprawdz_log() == True){
+		$user->usun_projekt($_GET['id']);
+		header('Location: projects');
+	}
+}
+else if(isset($_GET['page'])){
 	$kindOfPage = $_GET['page'];
 }
 else{

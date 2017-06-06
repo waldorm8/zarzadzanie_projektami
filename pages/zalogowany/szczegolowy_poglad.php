1999c4b56f5c76fe4d41321233aa65e7655dfc8c
piotrek31493 <piotrek31493@wp.pl>
2017-06-06 20:34:58 +0200

    Update szczegolowy_poglad.php

--- pages/zalogowany/szczegolowy_poglad.php
+++ pages/zalogowany/szczegolowy_poglad.php
@@ -38,32 +38,38 @@ if($user -> sprawdz_log() == True){
             <div class="col-lg-12" style="width:102%; margin-left:-1%;">
                     <div class="panel panel-default">
                         <div class="panel-heading" style="background:#00bff3 !important; color:white;">
-                            Zadania
+                            Sprinty i zadania
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <!-- Nav tabs -->
                             <ul class="nav nav-tabs">
-                                <li class="active"><a href="#zadanie1" data-toggle="tab">Zadanie 1</a>
+                                <li class="active"><a href="#zadanie1" data-toggle="tab">Sprint 1</a>
                                 </li>
-                                <li><a href="#zadanie2" data-toggle="tab">Zadanie 2</a>
+                                <li><a href="#zadanie2" data-toggle="tab">Sprint 2</a>
                                 </li>
-                                <li><a href="#zadanie3" data-toggle="tab">Zadanie 3</a>
+                                <li><a href="#zadanie3" data-toggle="tab">Sprint 3</a>
                                 </li>
                             </ul>
 
                             <!-- Tab panes -->
                             <div class="tab-content">
                                 <div class="tab-pane fade in active" id="zadanie1">
-                                    <h4>User 1, User 3</h4>
+                                    <h3>Zadanie 1</h3><h4>Tomek, Arek, Piotrek, Krystian</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <h3>Zadanie 2</h3><h4>Tomek, Arek, Piotrek, Krystian</h4>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                                 </div>
                                 <div class="tab-pane fade" id="zadanie2">
-                                    <h4>User 2, User 3</h4>
+                                    <h3>Zadanie 1</h3><h4>Tomek, Arek, Piotrek, Krystian</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <h3>Zadanie 2</h3><h4>Tomek, Arek, Piotrek, Krystian</h4>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                                 </div>
                                 <div class="tab-pane fade" id="zadanie3">
-                                    <h4>User 3, User 1</h4>
+                                    <h3>Zadanie 1</h3><h4>Tomek, Arek, Piotrek, Krystian</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <h3>Zadanie 2</h3><h4>Tomek, Arek, Piotrek, Krystian</h4>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                                 </div>
                             </div>
@@ -74,10 +80,8 @@ if($user -> sprawdz_log() == True){
                 <div class="col-lg-12" style="margin-left: -2%;">
                 <div class="panel-body">
                 <p>
-                    <a href="new_user.html"><button type="button" class="btn btn-primary btn-lg">Dodaj użytkownika do projektu</button></a>
-                    <a href="delete_user.html"><button type="button" class="btn btn-primary btn-lg">Usuń użytkownika z projektu</button></a>
-                    <a href="index.php?usunieto&id=><button type="button" class="btn btn-primary btn-lg">Usuń cały projekt</button></a>
-                    <a href=""><button type="button" class="btn btn-primary btn-lg">Edytuj projekt</button></a>
+                     <a href="sprint.html"><button type="button" class="btn btn-primary btn-lg btn-block" style="width: 104%;">Dodaj nowego sprinta</button></a>
+                    
                 </p>
                 </div>
                 </div>
